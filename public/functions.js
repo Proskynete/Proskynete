06c25c1cb2e86300048ed7db92f10be3f71ff5b7
Eduardo Álvarez <eduardo.a1993@gmail.com>
2021-02-12 12:13:01 +0000

    [Cron-Job] Update README with latest info

--- public/functions.js
+++ public/functions.js
@@ -52,7 +52,7 @@ moment_1.default.locale('en');
  * @param {GetVersionInterface} url - Url to check.
  * @returns All results according to search.
  */
-exports.getVersion = function (url) { return __awaiter(void 0, void 0, void 0, function () {
+var getVersion = function (url) { return __awaiter(void 0, void 0, void 0, function () {
     var file;
     return __generator(this, function (_a) {
         switch (_a.label) {
@@ -66,27 +66,30 @@ exports.getVersion = function (url) { return __awaiter(void 0, void 0, void 0, f
         }
     });
 }); };
+exports.getVersion = getVersion;
 /**
  * Get all articles from some RSS page.
  * @returns All items found.
  */
-exports.getLatestArticles = function () { return __awaiter(void 0, void 0, void 0, function () { return __generator(this, function (_a) {
+var getLatestArticles = function () { return __awaiter(void 0, void 0, void 0, function () { return __generator(this, function (_a) {
     return [2 /*return*/, parser.parseURL(constants_1.URLS.RSS).then(function (data) { return data.items; })];
 }); }); };
+exports.getLatestArticles = getLatestArticles;
 /**
  * Transform the date that we pass to a LL format (moment reference).
  * @param {string} date - Any format of date.
  * @returns format example: MM DD, YYYY
  */
-exports.prettyDate = function (date) {
+var prettyDate = function (date) {
     return moment_1.default(new Date(date)).format('LL');
 };
+exports.prettyDate = prettyDate;
 /**
  * Get an array of articles and transform them with a markdown format.
  * @param {array} articles - Articles obtained from an RSS.
  * @returns Link with the title, and the date of the post, with markdown syntax.
  */
-exports.sliceArticles = function (articles) {
+var sliceArticles = function (articles) {
     return articles
         .slice(0, constants_1.NUMBERS.ARTICLES)
         .map(function (_a) {
@@ -97,11 +100,12 @@ exports.sliceArticles = function (articles) {
     })
         .join('\n');
 };
+exports.sliceArticles = sliceArticles;
 /**
  * Get images from any instagram profile
  * @returns A object with permalink and media_url attributes
  */
-exports.getInstagramImages = function () { return __awaiter(void 0, void 0, void 0, function () {
+var getInstagramImages = function () { return __awaiter(void 0, void 0, void 0, function () {
     var data, edges;
     return __generator(this, function (_a) {
         switch (_a.label) {
@@ -120,12 +124,13 @@ exports.getInstagramImages = function () { return __awaiter(void 0, void 0, void
         }
     });
 }); };
+exports.getInstagramImages = getInstagramImages;
 /**
  * Trasnform the string of images to mdx format and slice the result
  * @param {InstagramImagesResponse[]} images - Array of { permalink, media_url } attributes
  * @returns An array of links wirth images obtained from instagram
  */
-exports.latestInstagramImages = function (images) {
+var latestInstagramImages = function (images) {
     return images
         .slice(0, constants_1.NUMBERS.IMAGES)
         .map(function (_a) {
@@ -134,9 +139,11 @@ exports.latestInstagramImages = function (images) {
     })
         .join('');
 };
+exports.latestInstagramImages = latestInstagramImages;
 /**
  * Get the number of years from the year of birth to now
  * @returns Number of years
  */
-exports.getYearsOld = function () { return moment_1.default().diff(constants_1.YEAR_OF_BIRTH, 'years'); };
+var getYearsOld = function () { return moment_1.default().diff(constants_1.YEAR_OF_BIRTH, 'years'); };
+exports.getYearsOld = getYearsOld;
 //# sourceMappingURL=functions.js.map
