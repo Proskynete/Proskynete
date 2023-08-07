ecd9a9681eb07b32e453da69c92c733b9024a77b
Eduardo Esteban Alvarez Castañeda <soy@eduardoalvarez.dev>
2023-08-07 09:49:57 -0400

    fix: change default branch name

--- src/constants.ts
+++ src/constants.ts
@@ -39,7 +39,7 @@ export const COUNT: CountInterface = {
 export const BASE_URL: BaseUrlInterface = {
 	NPM: 'https://www.npmjs.com/package',
 	WEBSITE: 'https://eduardoalvarez.dev',
-	TECHNOLOGIES: 'https://github.com/Proskynete/Proskynete/blob/master/images/icons',
+	TECHNOLOGIES: 'https://github.com/Proskynete/Proskynete/blob/main/images/icons',
 	INSTAGRAM_API: 'https://instagram-scraper-2022.p.rapidapi.com/ig/posts_username/',
 };
 
